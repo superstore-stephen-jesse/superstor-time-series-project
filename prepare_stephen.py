3a5ae0de1eb91b01242216c06eda9ce5c1d08de6
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-04-19 23:29:50 -0500

    Initial Commit (Stephen)

--- prepare_stephen.py
+++ prepare_stephen.py
@@ -9,8 +9,20 @@ from acquire_stephen import get_superstore_data
 # import acquire_jesse
 
 def prepare_superstore_data():
-    
+    # Call the acquire data data function
     df = get_superstore_data()
+    
+    # Lower camel-cased df columns names lower
+    df = df.rename(columns = str.lower)
+    
+    # Strip spaces in columns names 
+    df.columns = df.columns.str.replace(' ','_')
+    
+    # Drop Unnecessessary columns 
+#     df = df.drop(columns = ['','','','','']})
+    
+    # Engineer columns names 
+    
     
     # ========= NEED MORE PREPARE/CLEANING ===========
    
